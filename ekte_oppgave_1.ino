6005d330f8f0bf4de302966ad4311eeaae9133bc
Khuongh <60898006+Khuongh@users.noreply.github.com>
2020-02-10 21:11:02 +0100

    Add files via upload

---
+++ ekte_oppgave_1.ino
@@ -0,0 +1,50 @@
+/* Comment this out to disable prints and save space */
+#define BLYNK_PRINT SwSerial
+
+
+#include <SoftwareSerial.h>
+SoftwareSerial SwSerial(10, 11); // RX, TX
+
+#include <BlynkSimpleStream.h>
+
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = "wDjF8KndJs8pNw5El02b_ex7u6_6etO9";
+
+BlynkTimer timer;
+
+// This function sends Arduino's up time every second to Virtual Pin (5).
+// In the app, Widget's reading frequency should be set to PUSH. This means
+// that you define how often to send data to Blynk App.
+void myTimerEvent()
+{
+  // You can send any value at any time.
+  // Please don't send more that 10 values per second.
+  int pinVal = analogRead(A0);
+  float voltR1 = float(pinVal)*5/1024;
+  float voltR2=5-voltR1;
+  Blynk.virtualWrite(V0, voltR1);
+  Blynk.virtualWrite(V1, voltR2);
+  Serial.print("pikk");
+
+}
+
+void setup()
+{
+  // Debug console
+  SwSerial.begin(9600);
+
+  // Blynk will work through Serial
+  // Do not read or write this serial manually in your sketch
+  Serial.begin(9600);
+  Blynk.begin(Serial, auth);
+
+  // Setup a function to be called every second
+  timer.setInterval(1000L, myTimerEvent);
+}
+
+void loop()
+{
+  Blynk.run();
+  timer.run(); // Initiates BlynkTimer
+}
