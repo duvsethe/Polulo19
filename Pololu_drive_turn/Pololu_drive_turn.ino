371799e49b39df8c4f53b19dcf7bf0547bc2ee94
Khuongh <60898006+Khuongh@users.noreply.github.com>
2020-02-18 12:28:15 +0100

    added omment

---
+++ Pololu_drive_turn/Pololu_drive_turn.ino
@@ -0,0 +1,33 @@
+#include <Zumo32U4.h> //Including Zumo library
+
+//Calling motors, buttonA og LCD
+Zumo32U4Motors motors;
+Zumo32U4ButtonA buttonA;
+Zumo32U4LCD lcd;
+
+void setup() {
+  // writing to LCD
+  lcd.clear();
+  lcd.print("Press A");
+  lcd.gotoXY(0,1);
+  lcd.print("to start");
+  //wait for the button to be pressed
+  buttonA.waitForButton();
+  //waiting for the user to get away
+  delay(1000);
+  //clearing the LCD
+  lcd.clear();
+  Serial.println("hei");
+}
+
+void loop() {
+  //turning on the green led
+  ledGreen(1);
+  //making the robot drive forward
+  motors.setSpeeds(-100, 100);//Forward
+  delay(785);
+  motors.setSpeeds(0,0);
+  delay(200);
+
+
+ }
