cda8622a35d2e2cd42f7bb0bd74a682e9480ff58
Khuongh <60898006+Khuongh@users.noreply.github.com>
2020-04-10 12:13:58 +0200

    Create Pololu_cpr_Count.ino

---
+++ Pololu_cpr_Count/Pololu_cpr_Count.ino
@@ -0,0 +1,104 @@
+#include <Zumo32U4.h> //Including Zumo library
+#include<math.h>  //Including math library
+
+//Calling motors, buttonA, LCD and linesensor
+Zumo32U4Motors motors;
+Zumo32U4ButtonA buttonA;
+Zumo32U4LCD lcd;
+Zumo32U4LineSensors linesensor;
+Zumo32U4Buzzer buzzer;
+Zumo32U4Encoders encoder;
+
+//Creating variables
+float count;
+float meters = 0;  
+float batteryLife = 52.0;
+bool forward = false;
+bool backward = false;
+
+
+void setup() {
+  //Buzzer sound for when upload is finished
+  buzzer.play(">g32>>c32");
+  // writing to LCD
+  lcd.clear();
+  lcd.print("Press A");
+  lcd.gotoXY(0,1);
+  lcd.print("to start");
+  //wait for the button to be pressed
+  buttonA.waitForButton();
+  //waiting for the user to get away
+  delay(1000);
+  lcd.clear();
+  encoder.init();
+
+}
+
+void loop() {
+
+  //Calling function
+  count = distanceCounter();
+  meters += count;
+  batteryDrain();
+  batteryCharge();
+  dirDrive();
+  //Printing meters
+  lcd.gotoXY(0,0);
+  lcd.print(batteryLife);
+  delay(10);
+
+  }
+  
+
+
+float distanceCounter(){
+  //Measuring CPR count 
+  int leftCPR = encoder.getCountsAndResetLeft();
+  int rightCPR = encoder.getCountsAndResetRight();
+  //Converting CPR count to meters
+  float leftCounter = leftCPR * 0.000125;
+  float rightCounter = rightCPR * 0.000125;
+  //Calculating the average of both motors 
+  float distance = (leftCounter + rightCounter) / 2;
+
+  return distance;
+}
+
+float batteryDrain(){
+  //Draining battery life
+  if(count > 0) batteryLife = batteryLife - count;  
+}
+ 
+float batteryCharge(){
+  //Recharging battery life
+  if(count < 0 ) batteryLife = batteryLife - count;
+
+}
+
+void dirDrive(){
+    //Makes motor drive straight
+  if(batteryLife >= 51 && forward == false && backward == false){
+    motors.setSpeeds(100,100);
+    forward = true;
+    backward = false;
+  }
+  else if( batteryLife <= 50 && forward == true && backward == false){
+    motors.setSpeeds(0,0);
+    delay(20);
+    forward = false;
+    backward = false;
+  }
+  else if( batteryLife <= 50 && forward == false && backward == false){
+    motors.setSpeeds(-100,-100);
+    forward = false;
+    backward = true;
+    
+  }
+  else if ( batteryLife >= 53 && forward == false && backward == true){
+    motors.setSpeeds(0,0);
+    delay(20);   
+    forward = false;
+    backward = false;
+  }
+  
+}
