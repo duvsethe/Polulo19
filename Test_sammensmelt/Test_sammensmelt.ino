e13442b35bf00916c5244e95888ac7e496fe4de9
Khuongh <60898006+Khuongh@users.noreply.github.com>
2020-04-18 11:05:04 +0200

    Update Test_sammensmelt.ino

--- Test_sammensmelt/Test_sammensmelt.ino
+++ Test_sammensmelt/Test_sammensmelt.ino
@@ -350,8 +350,14 @@ void action21(){
     leftSpeed = constrain(leftSpeed, 0, (int)maxSpeed);
     rightSpeed = constrain(rightSpeed, 0, (int)maxSpeed);
     //Motor output
-    if ( linesensorValues[0] >= 800 && linesensorValues[1] >= 800 && linesensorValues[2] >= 800 && linesensorValues[3] >=800 && linesensorValues[4] >=800 && !buttonB.isPressed ){
-     blackTape();
+    if ( linesensorValues[0] >= 800 && linesensorValues[1] >= 800 && linesensorValues[2] >= 800 && linesensorValues[3] >=800 && linesensorValues[4] >=800){
+      motors.setSpeeds(0,0);
+      delay(1000);
+      if ( buttonB.isPressed()){
+      linePID = false;  
+      break;
+    }
+      blackTape();
      }
     motors.setSpeeds(leftSpeed, rightSpeed);
     //Prints linesensors on lcd
